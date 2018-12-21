ff7c2b942cf0f38e5d557b6c87b6d79c504807bc
postolenko <denis.postolenko@gmail.com>
2018-12-21 15:34:14 +0200

    Family Satin

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # http://postolenko.github.io/familysatin/index.html
 # http://postolenko.github.io/familysatin/card.html
+# http://postolenko.github.io/familysatin/order.html
--- js/scripts.js
+++ js/scripts.js
@@ -298,6 +298,64 @@ $(document).ready(function() {
 
     // ---------------------------------------
 
+    $(".tabs").each(function() {
+
+        $(this).find(".tab-link").each(function() {
+
+            if( $(this).hasClass("active") ) {
+
+                indexActiveTab = $(this).index(".tab-link");
+
+                $(this).click();
+
+                return false;
+
+            } else {
+
+                indexActiveTab = 0;
+
+            }
+
+        });
+
+        attrForTabLink = $(this).find(".tab-link").eq(indexActiveTab).attr("for");
+        activeTabRadio = $(this).find(".radio-tab[id = '"+ attrForTabLink +"']");
+        activeTabRadio.prop("checked", true);
+        $(this).find(".tab-link").eq(indexActiveTab).addClass("active");
+
+    });
+
+    $(".tab-link").click(function (e) {
+
+        if( $(this).hasClass("active") ) {
+
+            e.preventDefault();
+
+        } else {
+
+            tabsParent = $(this).closest(".tabs");
+            attrForTabLink = $(this).attr("for");
+            activeTabRadio = tabsParent.find(".radio-tab[id = '"+ attrForTabLink +"']");
+            activeTabRadio.prop("checked", true);
+
+            tabsParent.find(".tab-link").each(function () {
+                
+                if( $(this).hasClass("active") ) {
+
+                    $(this).removeClass("active");
+
+                }
+
+            });
+
+            $(this).addClass("active");
+
+        }
+
+    });
+
+    // -------------------------
+
 });
 
 function getFooterPosition() {
