baf9e279bbb2b21c88ce06ffb79610cc04e2c042
postolenko <denis.postolenko@gmail.com>
2018-12-30 18:09:49 +0200

    Family Satin

--- js/additional_scripts.js
+++ js/additional_scripts.js
@@ -64,7 +64,7 @@ $(document).ready(function() {
         dots: false,
         arrows: true,
         autoplay: true,
-        // autoplaySpeed: 5000,
+        autoplaySpeed: 5000,
         speed: 1200,
         slidesToShow: 1,
         slidesToScroll: 1
